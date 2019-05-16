ac02d97744434eda4fd9b1534b1a9346586d519c
malinipathakota <39076850+malinipathakota@users.noreply.github.com>
2019-05-15 18:17:19 -0700

    edited indentation

--- test/tps_extra_tests.c
+++ test/tps_extra_tests.c
@@ -14,7 +14,7 @@ void test_init()
 {
 	assert(tps_init(0) == 0);
 	assert(tps_init(0) == -1);
-    printf("test_init passed\n");
+    	printf("test_init passed\n");
 }
 
 void test_create_destroy()
@@ -22,14 +22,14 @@ void test_create_destroy()
 	assert(tps_destroy() == -1);
 	assert(tps_create() == 0);
 	assert(tps_destroy() == 0);
-    printf("test_create_destroy passed\n");
+    	printf("test_create_destroy passed\n");
 }
 
 void no_create()
 {
 	assert(tps_read(0,TPS_SIZE, msg1) == -1);
 	assert(tps_write(0,TPS_SIZE, msg1) == -1);
-    printf("no_create passed \n");
+    	printf("no_create passed \n");
 }
 
 void no_buffer()
@@ -37,7 +37,7 @@ void no_buffer()
 	char buffer;
 	assert(tps_read(0, 1 , &buffer) == -1);
 	assert(tps_write(0, 1, &buffer) == -1);
-    printf("no buffer passed \n");
+    	printf("no buffer passed \n");
 }
 
 void bounds()
@@ -47,7 +47,7 @@ void bounds()
 	assert(tps_read(0, TPS_SIZE + 1, buffer) == -1);
 	assert(tps_write(0, TPS_SIZE + 1, buffer) == -1);
 	tps_destroy();
-    printf("bounds passed \n");
+    	printf("bounds passed \n");
 }
 
 void null_tps_read_write()
@@ -56,7 +56,7 @@ void null_tps_read_write()
 	assert(tps_read(0, 16, NULL)== -1);
 	assert(tps_write(0, 16, NULL) == -1);
 	tps_destroy();
-    printf("null_tps_read_write passed \n");
+    	printf("null_tps_read_write passed \n");
 }
 
 int main(int argc, char **argv)
@@ -67,7 +67,7 @@ int main(int argc, char **argv)
 	null_tps_read_write();
 	bounds();
 	no_buffer();
-    printf("All tests passed!\n");
+    	printf("All tests passed!\n");
 	return 0;
 }
 
