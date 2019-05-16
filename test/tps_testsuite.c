225cd3336f4801d806d3aaaeb84b880e53bd68b0
malinipathakota <39076850+malinipathakota@users.noreply.github.com>
2019-05-15 18:18:02 -0700

    edited indentation

--- test/tps_testsuite.c
+++ test/tps_testsuite.c
@@ -14,16 +14,16 @@ void *latest_mmap_addr;
 void *__real_mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off);
 void *__wrap_mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
 {
-    latest_mmap_addr = __real_mmap(addr, len, prot, flags, fildes, off);
-    return latest_mmap_addr;
+	latest_mmap_addr = __real_mmap(addr, len, prot, flags, fildes, off);
+	return latest_mmap_addr;
 }
 
 int main(int argc, char **argv)
 {
-    tps_init(1);
-    tps_create();
-    char *tps_addr = latest_mmap_addr;
-    tps_addr[0] = '\0';
+	tps_init(1);
+	tps_create();
+	char *tps_addr = latest_mmap_addr;
+	tps_addr[0] = '\0';
 	return 0;
 }
 
